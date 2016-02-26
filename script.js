ffb99e1c0e479ad9b0ff3f941b236ae874b58761
Tom Moore <tom5265@gmail.com>
2016-02-26 10:33:21 -0600

    added random color and hover

--- script.js
+++ script.js
@@ -17,14 +17,31 @@ function addDiv() {
     newDiv.className = 'blackBox';
     document.body.appendChild(newDiv); 
     newDiv.id = String(divAmount);
-    newDiv.onmouseover = addHeader;    
+    newDiv.onmouseover = addHeader;  
+    newDiv.onmouseleave = removeHeader;  
+    newDiv.onclick = getRandomColor;
     
     
     function addHeader (e) {
-    var hoveredElement = e.target;
-    hoveredElement.innerHTML = newDiv.id;
+      var hoveredElement = e.target;
+      hoveredElement.innerHTML = newDiv.id;
 }    
+   function removeHeader (e) {
+    var hoveredElement = e.target;
+    hoveredElement.innerHTML = '';
+}  
+
 }
+
+function getRandomColor(e) {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    e.target.style.backgroundColor = color;
+}
+
 
 
    
